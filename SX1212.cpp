9b3dae645e98eeed2d2d021983dd40bfcef77ec7
chisl <info@chisl.io>
2018-08-02 07:35:50 +0200

    Initial commit

---
+++ SX1212.cpp
@@ -0,0 +1,16 @@
+/*
+ * name:        SX1212
+ * description: Ultra-Low Power Integrated 300-510MHz Transceiver
+ * manuf:       Semtech
+ * version:     Version 0.1
+ * url:         https://www.semtech.com/uploads/documents/sx1272.pdf
+ * date:        2018-07-31
+ * author       https://chisl.io/
+ * file:        SX1212.cpp
+ * types:       RF
+ */
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
+#include "SX1212.hpp"
